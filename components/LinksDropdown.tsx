75412fc9ffdf701ce68039606139eb5d36e547ab
Welinton Fernadnes <portfolio.fernandes@gmail.com>
2024-11-26 11:40:27 -0300

    Dropdownlinks Component set done

--- components/LinksDropdown.tsx
+++ components/LinksDropdown.tsx
@@ -1,6 +1,35 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger, 
+} from '@/components/ui/dropdown-menu';
+import { AlignCenter, AlignLeft, Icon } from 'lucide-react';
+import { Button } from './ui/button';
+import links from '@/utils/links';
+import Link from 'next/link';
+
 function LinksDropdown() {
   return (
-    <h2 className="text-4xl">LinksDropdown</h2>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className='lg:hidden'>
+        <Button variant={'outline'} size={`icon`}>
+          <AlignLeft />
+          <span className='sr-only'>Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-52 lg:hidden' align='start' sideOffset={25}>
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.href}>
+              <Link href={link.href} className='flex items-center gap-x-2'>
+                {link.icon} <span className='capitalize'>{link.label}</span>
+              </Link>
+            </DropdownMenuItem>
+          )
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
